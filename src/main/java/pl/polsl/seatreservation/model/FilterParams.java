2695fe21a15e7db7d8b61566837596ba7029fac7
Piotr Kuźnik <piotr.damian.kuznik@gmail.com>
2020-12-12 12:48:02 +0100

    Wsteczna zgodność z trybem interaktywnym w konsoli

--- src/main/java/pl/polsl/seatreservation/model/FilterParams.java
+++ src/main/java/pl/polsl/seatreservation/model/FilterParams.java
@@ -1,13 +1,11 @@
-
 package pl.polsl.seatreservation.model;
 
 import java.util.*;
 
 /**
- * Model responsible for preparing the output data. 
- * the output can come from the command line (program startup), 
- * but it can also be retrieved from the user
- * 
+ * Model responsible for preparing the output data. the output can come from the
+ * command line (program startup), but it can also be retrieved from the user
+ *
  * @author Piotr Kuźnik
  * @version 3.0
  */
@@ -17,64 +15,73 @@ public class FilterParams {
      * Object contains final parameters for program
      */
     private final Parameters parameters;
-    
+
     /**
      * Construct
+     *
      * @param args Array with begin parameters of program
      */
     public FilterParams(String args[]) {
         int sizeX = 10, sizeY = 10, spaceX = 0, spaceY = 0, mode = Parameters.UI_MODE_DEFAULT;
         List<Integer> quantityOfChairsToReserve = new ArrayList<>();
-        
+        InputData input = new InputData();
+
+        if (this.inArray("-mode", args)) {
+            mode = this.readArgumentFromArray("-mode", args);
+            args = this.removeArgumentFromArray("-mode", args);
+
+            if (mode != Parameters.UI_MODE_CONSOLE && mode != Parameters.UI_MODE_GRAPHIC) {
+                //set deafult
+                mode = Parameters.UI_MODE_DEFAULT;
+            }
+        }
+
         if (this.inArray("-sizeX", args)) {
             sizeX = this.readArgumentFromArray("-sizeX", args);
             args = this.removeArgumentFromArray("-sizeX", args);
+        } else if (mode == Parameters.UI_MODE_CONSOLE) {
+            sizeX = input.readWidthHall();
         }
-        
-       
+
         if (this.inArray("-sizeY", args)) {
             sizeY = this.readArgumentFromArray("-sizeY", args);
             args = this.removeArgumentFromArray("-sizeY", args);
-        } 
-        
+        } else if (mode == Parameters.UI_MODE_CONSOLE) {
+            sizeX = input.readHeightHall();
+        }
+
         if (this.inArray("-spaceX", args)) {
             spaceX = this.readArgumentFromArray("-spaceX", args);
             args = this.removeArgumentFromArray("-spaceX", args);
         }
-        
+
         if (this.inArray("-spaceY", args)) {
             spaceY = this.readArgumentFromArray("-spaceY", args);
             args = this.removeArgumentFromArray("-spaceY", args);
         }
-        
-        if (this.inArray("-mode", args)) {
-            mode = this.readArgumentFromArray("-mode", args);
-            args = this.removeArgumentFromArray("-mode", args);
-            
-            if (mode != Parameters.UI_MODE_CONSOLE && mode != Parameters.UI_MODE_GRAPHIC) {
-                //set deafult
-                mode = Parameters.UI_MODE_DEFAULT; 
-            }
+
+        if (mode == Parameters.UI_MODE_CONSOLE && args.length == 0) {
+            args = input.readQuantityOfChairsToReserve();
         }
-       
+
         for (String arg : args) {
             quantityOfChairsToReserve.add(Integer.parseInt(arg));
         }
-            
+
         this.parameters = new Parameters(sizeX, sizeY, spaceX, spaceY, quantityOfChairsToReserve, mode);
     }
-    
+
     /**
-     * 
-     * @return Object with final parameter for program 
+     *
+     * @return Object with final parameter for program
      */
     public Parameters getParameters() {
         return this.parameters;
     }
-    
+
     /**
      * check value in array
-     * 
+     *
      * @param argName searching argument
      * @param args array of arguments
      * @return True if exists
@@ -82,42 +89,45 @@ public class FilterParams {
     private boolean inArray(String argName, String args[]) {
         return Arrays.stream(args).anyMatch(argName::equals);
     }
-    
+
     /**
      * Return value from argument name
+     *
      * @param argName
      * @param args
      * @return value search arguments type integer
      */
     private int readArgumentFromArray(String argName, String args[]) {
-        for (int i=0; i<args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
             if (args[i].equals(argName) && i + 1 < args.length) {
-                
+
                 return Integer.parseInt(args[i + 1]);
             }
         }
-        
+
         return 0;
     }
-    
+
     /**
      * Remove value and argument name from array
+     *
      * @param argRemove Name argument for remove from args
      * @param args Array of arguments
-     * @return New array without argument and value for remove  (index i and i + 1)
+     * @return New array without argument and value for remove (index i and i +
+     * 1)
      */
     private String[] removeArgumentFromArray(String argRemove, String args[]) {
         String[] newArgs = new String[args.length - 2];
         int j = 0;
-        for (int i=0; i<args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
             if (args[i].equals(argRemove) || (i > 0 && args[i - 1].equals(argRemove))) {
-               continue;
+                continue;
             }
-            
+
             newArgs[j++] = args[i];
-         
+
         }
-        
+
         return newArgs;
     }
 }
